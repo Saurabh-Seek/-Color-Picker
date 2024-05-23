954081f152212a05dfcd60e376118556eed92024
Saurabh kumar <shingkunal58@gmail.com>
2024-05-23 21:23:16 +0530

    dark

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,24 @@
 "use client"
+
 import React, { useState } from "react";
+
 export default function Home() {
-  const [color, setColor] = useState('#000000');
-  const handleColorChange = (e:any) => {
-    setColor(e.target.value);
-  };
+  
+  const  [dark, setDark] = useState(false)
+
+    const handleThem = () =>{
+        setDark((a) => !a);
+    }
   return (
     <>
-      <input type="color" value={color} onChange={handleColorChange} />
-      <div style={{ height: "500px", width: "500px", backgroundColor: color }}></div>
+    
+        <div style={{ height:"500px",widows:"500px",   backgroundColor:dark ? "#000":"#fff"}}>
+
+<span style={{color: dark ? "#fff":"#000"}}>light mode and darkmode</span>
+
+<button onClick={handleThem} className="ml-10 text-red-900">submit</button>
+      </div>
     </>
+   
   );
 }
